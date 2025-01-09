304635156f8046b0fe6ca265872ade8212585c84
Yevhenns <gifestum@gmail.com>
2025-01-09 14:00:48 +0200

    styling share popup

--- src/components/ui/PopupShareEvent.tsx
+++ src/components/ui/PopupShareEvent.tsx
@@ -1,8 +1,10 @@
 import { createPortal } from 'react-dom';
+import { BiCopy } from 'react-icons/bi';
 import { FaInstagram, FaViber } from 'react-icons/fa';
 import { FiFacebook } from 'react-icons/fi';
 import { IoMdClose } from 'react-icons/io';
 import { LiaTelegram } from 'react-icons/lia';
+import { useLocation } from 'react-router';
 
 import { usePreventScroll } from '@/hooks/usePreventScroll';
 
@@ -13,20 +15,34 @@ interface PopupShareEventProps {
 export const PopupShareEvent = ({ closePopup }: PopupShareEventProps) => {
   usePreventScroll();
 
+  const { pathname } = useLocation();
+
   return createPortal(
     <div className="fixed top-0 left-0 z-20 w-full h-full flex justify-center items-center bg-white bg-opacity-50">
-      <div className="bg-background flex flex-col gap-[24px] items-center rounded-[20px] border-lightPurple border-2">
-        <div className="flex px-[116px] py-[24px] border-b-lightPurple border-b-2">
-          <p>Поділитися подією з друзями</p>
-          <button onClick={closePopup}>
-            <IoMdClose />
+      <div className="min-w-[448px] relative bg-background flex flex-col items-center rounded-[20px] border-lightPurple border-[1px]">
+        <div className="flex w-full justify-center py-[16px] border-b-lightPurple border-b-[1px]">
+          <p className="text-[20px]">Поділитися подією з друзями</p>
+          <button
+            className="absolute right-[16px] top-[16px]"
+            onClick={closePopup}
+          >
+            <IoMdClose size={18} />
           </button>
         </div>
-        <div className="flex py-[24px]">
-          <LiaTelegram />
-          <FaInstagram />
-          <FiFacebook />
-          <FaViber />
+        <div className=" flex flex-col gap-[16px] items-center pt-[32px] pb-[16px] px-[86px]">
+          <div className="flex gap-[24px]">
+            <LiaTelegram size={32} />
+            <FaInstagram size={32} />
+            <FiFacebook size={32} />
+            <FaViber size={32} />
+          </div>
+          <div className="flex gap-2 justify-between items-center border-lightPurple border-[1px] rounded-[10px] p-[8px]">
+            <div>
+              <p>Event URL</p>
+              <p>https://evently-book.vercel.app{pathname}</p>
+            </div>
+            <BiCopy size={24} />
+          </div>
         </div>
       </div>
     </div>,
