de877e389c7ed46212cd97d544b48b64aaa60328
Yevhenns <gifestum@gmail.com>
2025-01-31 12:36:39 +0200

    hide border

--- src/components/layout/ProfileTabLayout.tsx
+++ src/components/layout/ProfileTabLayout.tsx
@@ -1,16 +1,23 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import { MainLines } from '../main/MainLines';
 import ProfileTab from '../tabProfile/ProfileTab';
 
 export const ProfileTabLayout = () => {
+  const { pathname } = useLocation();
+
+  const borderIsHidden =
+    pathname.includes('user_profile') || pathname.includes('favourite');
+
   return (
     <main className="flex gap-[25px] relative p-[0px_55px_55px_55px] ">
       <MainLines />
       <ProfileTab />
 
-      <section className="rounded-[20px] border border-buttonPurple border-spacing-8 py-4 px-[15px] w-full h-auto">
+      <section
+        className={`rounded-[20px] py-4 px-[15px] w-full h-auto ${borderIsHidden ? '' : 'border border-buttonPurple border-spacing-8'}`}
+      >
         <Suspense fallback={<div>Loading...</div>}>
           <Outlet />
         </Suspense>
