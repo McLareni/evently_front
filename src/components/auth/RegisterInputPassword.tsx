7468133bea4f4e13ce9b0c46cd596796705bcbc0
Yevhenns <gifestum@gmail.com>
2025-03-19 10:52:58 +0200

    only letters for user name

--- src/components/auth/RegisterInputPassword.tsx
+++ src/components/auth/RegisterInputPassword.tsx
@@ -38,14 +38,16 @@ export const RegisterInputPassword: React.FC<RegisterInputPasswordProps> = ({
     handleSubmit,
     watch,
     trigger,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<RegisterFormInputsPassword>({
     mode: 'onChange',
   });
 
-  const onSubmit = async (data: RegisterFormInputsPassword) => {
-    const { name, password, confirmPassword } = data;
-
+  const onSubmit = async ({
+    name,
+    password,
+    confirmPassword,
+  }: RegisterFormInputsPassword) => {
     setUserData(prev => ({ ...prev, password, name, confirmPassword }));
     setStatusAuth('confirm_email');
   };
@@ -59,7 +61,6 @@ export const RegisterInputPassword: React.FC<RegisterInputPasswordProps> = ({
 
   // Error handling on blur
   const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    // setErrorMessage('');
     const { name } = e.target;
     if (name === 'name') {
       trigger('name');
@@ -92,6 +93,7 @@ export const RegisterInputPassword: React.FC<RegisterInputPasswordProps> = ({
               isSubmitted={isSubmitted}
               onBlur={e => handleBlur(e)}
               register={register}
+              maxLength={40}
               validation={{ required: true, validate: validateName }}
               errors={errors}
             />
@@ -117,6 +119,7 @@ export const RegisterInputPassword: React.FC<RegisterInputPasswordProps> = ({
               isSubmitted={isSubmitted}
               onBlur={e => handleBlur(e)}
               register={register}
+              maxLength={40}
               validation={{ required: true, validate: validatePassword }}
               errors={errors}
             />
@@ -138,6 +141,7 @@ export const RegisterInputPassword: React.FC<RegisterInputPasswordProps> = ({
               isSubmitted={isSubmitted}
               onBlur={e => handleBlur(e)}
               register={register}
+              maxLength={40}
               validation={{
                 required: true,
                 validate: value => value === watch('password'),
@@ -160,6 +164,7 @@ export const RegisterInputPassword: React.FC<RegisterInputPasswordProps> = ({
             onClick={() => setIsSubmitted(true)}
             primary
             className={`w-[364px] mt-10 ml-auto mr-auto`}
+            disabled={!isValid}
           >
             Створити акаунт
           </SharedBtn>
