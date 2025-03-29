686384e0f8bef88dd51c955be9dcdddc30fa2808
Yevhenns <gifestum@gmail.com>
2025-03-29 14:42:23 +0200

    pdf playground

---
+++ src/PDF.tsx
@@ -0,0 +1,42 @@
+import {
+  Document,
+  Font,
+  Page,
+  StyleSheet,
+  Text,
+  View,
+} from '@react-pdf/renderer';
+
+Font.register({
+  family: 'Roboto',
+  src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf',
+});
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+  text: {
+    fontSize: 18,
+    margin: 12,
+    fontFamily: 'Roboto',
+  },
+});
+
+export const PDF = ({ user }: { user: User }) => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text style={styles.text}>Весела подія</Text>
+        <Text style={styles.text}>Ім&apos;я: {user.name}</Text>
+        <Text style={styles.text}>Номер телефону: {user.phoneNumber}</Text>
+      </View>
+    </Page>
+  </Document>
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,10 @@
+import { selectUser } from '@/redux/auth/selectors';
+import { useAppSelector } from '@/redux/hooks';
+
+import { PDF } from '@/PDF';
 import { useLazyGetAllEventsQueryWithTrigger } from '@/hooks/query/useLazyGetAllEventsQueryWithTrigger';
 import { useScrollToTop } from '@/hooks/useScrollToTop';
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 
 import { AllEvents } from '@/components/allEvents/AllEvents';
 import { Container } from '@/components/container/Container';
@@ -8,12 +13,15 @@ import { Hero } from '@/components/hero/Hero';
 import { Main } from '@/components/main/Main';
 import { Organizers } from '@/components/organizers/Organizers';
 import { TopEvents } from '@/components/topEvents/TopEvents';
+import Button from '@/components/ui/Button';
 import { ShowAllButton } from '@/components/ui/ShowAllButton';
 import Spinner from '@/components/ui/Spinner';
 
 const Home: React.FC = () => {
   const { events, isLoading } = useLazyGetAllEventsQueryWithTrigger({});
   useScrollToTop();
+
+  const user = useAppSelector(selectUser);
 
   const shownEvents = 16;
   const notTopEvents = events
@@ -26,6 +34,17 @@ const Home: React.FC = () => {
   return (
     <Main className="flex flex-col gap-16 z-10">
       <Hero />
+
+      <PDFViewer style={{ height: '800px' }}>
+        <PDF user={user} />
+      </PDFViewer>
+
+      <Button type="button">
+        <PDFDownloadLink document={<PDF user={user} />} fileName="ticket.pdf">
+          Скачати Pdf
+        </PDFDownloadLink>
+      </Button>
+
       <>
         <TopEvents filteredEvents={topEvents} />
         {notTopEvents && (
