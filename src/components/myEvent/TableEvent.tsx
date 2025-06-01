a11545846551d13a43275fca3e833dc52e50c93f
McLareni <mukola.kosobutski05@gmail.com>
2025-06-01 15:50:26 +0200

    page in myEvent

--- src/components/myEvent/TableEvent.tsx
+++ src/components/myEvent/TableEvent.tsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom';
 
 import { selectUser } from '@/redux/auth/selectors';
-import { useGetAllMyEventsQuery } from '@/redux/events/operations';
+import { useLazyGetAllMyEventsQuery } from '@/redux/events/operations';
 import { useAppSelector } from '@/redux/hooks';
 
 import clsx from 'clsx';
 
+import SmallSpinner from '../ui/SmallSpinner';
 import Spinner from '../ui/Spinner';
 import EventRow from './EventRow';
 import TableHead from './TableHead';
 
 const TableEvent = () => {
   const [idPopUp, setIdPopUp] = useState<string | undefined>(undefined);
+  const [events, setEvents] = useState<Event[]>([]);
+  const [isFullList, setIsFullList] = useState(false);
+  const { ref, inView } = useInView();
+  const [page, setPage] = useState(0);
   const { id } = useAppSelector(selectUser);
-  const { data: events, isFetching, refetch } = useGetAllMyEventsQuery(id);
+  const [getEvents, { isFetching, isLoading }] = useLazyGetAllMyEventsQuery();
 
   const handleOpenPopUp = (id?: string) => {
     setIdPopUp(id);
@@ -28,11 +34,54 @@ const TableEvent = () => {
     handleOpenPopUp(undefined);
   };
 
-  const handleRefreshEvents = async () => {
-    refetch();
+  useEffect(() => {
+    filterEvents();
+  }, []);
+
+  const filterEventsFn = async (pageN: number) => {
+    return await getEvents({
+      id,
+      page: pageN,
+    });
   };
 
-  if (isFetching) {
+  const filterEvents = async () => {
+    setIsFullList(false);
+
+    const response = await filterEventsFn(0);
+    setPage(1);
+
+    if (response.status === 'uninitialized') {
+      filterEvents();
+    }
+
+    setEvents(response.data || []);
+  };
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await filterEventsFn(page);
+
+      setPage(prev => prev + 1);
+      setEvents(prevEvents => [...prevEvents, ...(response.data || [])]);
+
+      if ((response?.data?.length || 0) < 9) {
+        console.log('Full list');
+
+        setIsFullList(true);
+      }
+    };
+    if (inView && !isLoading && !isFullList && !isFetching) {
+      fetchEvents();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+
+  const handleRefreshEvents = async () => {
+    filterEvents();
+  };
+
+  if (isFetching || isLoading) {
     return <Spinner />;
   }
 
@@ -53,29 +102,37 @@ const TableEvent = () => {
   }
 
   return (
-    <table
-      onClick={e => handleClickTable(e.target as HTMLElement)}
-      className="w-full border-collapse"
-    >
-      <TableHead refresh={handleRefreshEvents} />
-      <tbody className="">
-        {events.map((event, index) => (
-          <tr
-            key={event.id}
-            className={clsx(
-              'h-[100px] items-end relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full',
-              { 'after:bg-buttonPurple': index !== events.length - 1 }
-            )}
-          >
-            <EventRow
-              event={event}
-              popUpIsShow={event.id === idPopUp}
-              openPopUp={handleOpenPopUp}
-            />
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table
+        onClick={e => handleClickTable(e.target as HTMLElement)}
+        className="w-full border-collapse"
+      >
+        <TableHead refresh={handleRefreshEvents} />
+        <tbody className="">
+          {events.map((event, index) => (
+            <tr
+              key={event.id}
+              className={clsx(
+                'h-[100px] items-end relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full',
+                { 'after:bg-buttonPurple': index !== events.length - 1 }
+              )}
+            >
+              <EventRow
+                event={event}
+                popUpIsShow={event.id === idPopUp}
+                openPopUp={handleOpenPopUp}
+              />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {inView && !isFullList && (
+        <div>
+          <SmallSpinner />
+        </div>
+      )}
+      <div ref={ref} id="inView"></div>
+    </>
   );
 };
 
