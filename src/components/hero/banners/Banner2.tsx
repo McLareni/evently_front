ecb49a0b5aff0159b22b5be753f553a63a00aefb
McLareni <mukola.kosobutski05@gmail.com>
2025-09-08 17:08:29 +0200

    bunners and fix bug

--- src/components/hero/banners/Banner2.tsx
+++ src/components/hero/banners/Banner2.tsx
@@ -4,27 +4,27 @@ const Banner1 = () => {
   return (
     <div className="w-full h-full rounded-[20px] bg-bg-gradient p-[48px_70px_34px_59px]">
       <h1 className="text-8xl text-center mb-[56px] text-title">
-        Ми відкрилися!
+        Організовуєш події?
       </h1>
-      <div className="flex">
+      <div className="flex flex-row-reverse">
         <div className=" text-center">
           <h2 className="text-5xl text-title mb-8 mt-[6px]">
-            Шукай події, купуй квитки, створюй власне свято вже сьогодні!
+            Реєструйся та продавай квитки легко та швидко!
           </h2>
           <button
             className={`bg-dark-gradient w-[421px] h-12 rounded-[71px_8px] mx-auto text-background text-2xl
                 hover:shadow-shadowPrimaryBtn focus:outline-none active:shadow-primaryBtnActive`}
           >
-            Обрати подію
+            Створити подію
           </button>
         </div>
-        <div className='relative'>
+        <div className="relative">
           <Lottie
-          src="https://cdn.lottielab.com/l/5SmcsiuKdxyQoa.json"
-          autoplay
-          className="w-[400px] bg-transparent -mt-16"
-        />
-        <div className='w-32 h-12'></div>
+            src="https://cdn.lottielab.com/l/5SmcsiuKdxyQoa.json"
+            autoplay
+            className="w-[400px] bg-transparent -mt-16"
+          />
+          <div className="w-32 h-12"></div>
         </div>
       </div>
     </div>
--- src/components/hero/banners/Banner3.tsx
+++ src/components/hero/banners/Banner3.tsx
@@ -4,27 +4,27 @@ const Banner1 = () => {
   return (
     <div className="w-full h-full rounded-[20px] bg-bg-gradient p-[48px_70px_34px_59px]">
       <h1 className="text-8xl text-center mb-[56px] text-title">
-        Ми відкрилися!
+        Квитки — це просто!
       </h1>
       <div className="flex">
         <div className=" text-center">
           <h2 className="text-5xl text-title mb-8 mt-[6px]">
-            Шукай події, купуй квитки, створюй власне свято вже сьогодні!
+            Все просто: створюй подію, керуй учасниками, продавай квитки.
           </h2>
           <button
             className={`bg-dark-gradient w-[421px] h-12 rounded-[71px_8px] mx-auto text-background text-2xl
                 hover:shadow-shadowPrimaryBtn focus:outline-none active:shadow-primaryBtnActive`}
           >
-            Обрати подію
+            Як це працює?
           </button>
         </div>
-        <div className='relative'>
+        <div className="relative">
           <Lottie
-          src="https://cdn.lottielab.com/l/5SmcsiuKdxyQoa.json"
-          autoplay
-          className="w-[400px] bg-transparent -mt-16"
-        />
-        <div className='w-32 h-12'></div>
+            src="https://cdn.lottielab.com/l/5SmcsiuKdxyQoa.json"
+            autoplay
+            className="w-[400px] bg-transparent -mt-16"
+          />
+          <div className="w-32 h-12"></div>
         </div>
       </div>
     </div>
